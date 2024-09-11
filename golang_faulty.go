bd19b1718159255ee1e4b4fa4e88983afe2ae9fa
Jody McIntyre <scjody@modernduck.com>
2024-09-11 13:38:28 -0400

    Extract timestamp and message
    For consistency with the Python version, we extract the timestamp and
    message, and send them as separate parts of the JSON payload.

--- golang_faulty.go
+++ golang_faulty.go
@@ -8,7 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
+	"regexp"
 	"sync"
 )
 
@@ -30,59 +30,64 @@ func readAndSendLogs(filePath, apiURL string) {
 	// Using a Scanner is a good idea here since it offers a better interface
 	// than Reader in this use case.
 	scanner := bufio.NewScanner(file)
+	re := regexp.MustCompile(`\[(.*)] ERROR (.*)`)
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		// This is a change of functionality from the Python version.  Here we
-		// just look for the text ERROR and then we send the entire line.
-		// The Python version performed a more strict check using a regular
-		// expression and also parsed the fields into a timestamp and message.
-		// I don't know if this change is intentional (maybe requirements have
-		// changed) but I'll look into making this consistent with the Python
-		// version in an upcoming commit.
-		if strings.Contains(line, "ERROR") {
-			wg.Add(1)
-			go func() {
-				// Sending log to API
-				defer wg.Done()
+		// Originally we just looked for the text ERROR and sent the entire
+		// line; this was a change of functionality from the Python version.
+		// I've changed it to use a regular expression to perform a more strict
+		// check and parse fields into a timestamp and message.
 
-				// I ran the program and it failed due to an error from the backend:
-				// `SyntaxError: Unexpected token W in JSON at position 1`
-				// Looking back at the code, the issue is obvious: we're sending
-				// the raw string rather than JSON.
-				data := map[string]string{"timestamp": "TODO", "message": line}
-				jsonData, err := json.Marshal(data)
-				if err != nil {
-					// TODO BEFORE MERGE: fix error handling (along with the others)
-					fmt.Println("Failed to encode JSON:", err)
-				}
-
-				resp, err := http.Post(apiURL, "application/json", bytes.NewBuffer(jsonData))
-				// As well as checking for errors from the Post() call, we could
-				// also check the status code of the response, as in the Python
-				// version.  I'll add that in an upcoming commit.
-				if err != nil {
-					// As with the Python version, we should send errors to STDERR.
-					// Unfortunately, we can't easily exit with an error status
-					// without some refactoring - we'd need a mechanism (e.g.
-					// a channel) to communicate the failure in the goroutine
-					// back to the main program.
-					fmt.Println("Failed to send log:", err)
-				} else {
-					// As above, this is an unhandled error that I'm comfortable with
-					defer resp.Body.Close()
-					// I believe ReadAll is deprecated. I'll look into replacing
-					// it in an upcoming commit.
-					body, _ := ioutil.ReadAll(resp.Body)
-					fmt.Printf("Response: %s\n", string(body))
-				}
-				// I noticed while working on the code that the wg.Done()
-				// call here will only happen on success, leading to the
-				// main goroutine blocking if any errors happen.  I've fixed
-				// this using `defer`.
-				//wg.Done()
-			}()
+		match := re.FindStringSubmatch(line)
+		if match == nil {
+			continue
 		}
+
+		wg.Add(1)
+		go func() {
+			// Sending log to API
+			defer wg.Done()
+
+			fmt.Printf("Error at %s: %s\n", match[1], match[2])
+
+			// I ran the program and it failed due to an error from the backend:
+			// `SyntaxError: Unexpected token W in JSON at position 1`
+			// Looking back at the code, the issue is obvious: we're sending
+			// the raw string rather than JSON.
+			data := map[string]string{"timestamp": match[1], "message": match[2]}
+			jsonData, err := json.Marshal(data)
+			if err != nil {
+				// TODO BEFORE MERGE: fix error handling (along with the others)
+				fmt.Println("Failed to encode JSON:", err)
+			}
+
+			resp, err := http.Post(apiURL, "application/json", bytes.NewBuffer(jsonData))
+			// As well as checking for errors from the Post() call, we could
+			// also check the status code of the response, as in the Python
+			// version.  I'll add that in an upcoming commit.
+			if err != nil {
+				// As with the Python version, we should send errors to STDERR.
+				// Unfortunately, we can't easily exit with an error status
+				// without some refactoring - we'd need a mechanism (e.g.
+				// a channel) to communicate the failure in the goroutine
+				// back to the main program.
+				fmt.Println("Failed to send log:", err)
+			} else {
+				// As above, this is an unhandled error that I'm comfortable with
+				defer resp.Body.Close()
+				// I believe ReadAll is deprecated. I'll look into replacing
+				// it in an upcoming commit.
+				body, _ := ioutil.ReadAll(resp.Body)
+				fmt.Printf("Response: %s\n", string(body))
+			}
+			// I noticed while working on the code that the wg.Done()
+			// call here will only happen on success, leading to the
+			// main goroutine blocking if any errors happen.  I've fixed
+			// this using `defer`.
+			//wg.Done()
+		}()
+
 	}
 
 	if err := scanner.Err(); err != nil {
