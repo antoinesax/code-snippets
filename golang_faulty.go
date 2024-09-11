82a311d26763138cdbc0512d52041f711b40b997
Jody McIntyre <scjody@modernduck.com>
2024-09-11 11:30:40 -0400

    reformat via `go fmt`

--- golang_faulty.go
+++ golang_faulty.go
@@ -1,51 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "strings"
-    "sync"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
 )
 
 func readAndSendLogs(filePath, apiURL string) {
-    file, err := os.Open(filePath)
-    if err != nil {
-        fmt.Println("Error opening file:", err)
-        return
-    }
-    defer file.Close()
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
 
-    var wg sync.WaitGroup
-    scanner := bufio.NewScanner(file)
+	var wg sync.WaitGroup
+	scanner := bufio.NewScanner(file)
 
-    for scanner.Scan() {
-        line := scanner.Text()
-        if strings.Contains(line, "ERROR") {
-            wg.Add(1)
-            go func() {
-                // Sending log to API
-                resp, err := http.Post(apiURL, "application/json", strings.NewReader(line))
-                if err != nil {
-                    fmt.Println("Failed to send log:", err)
-                } else {
-                    defer resp.Body.Close()
-                    body, _ := ioutil.ReadAll(resp.Body)
-                    fmt.Printf("Response: %s\n", string(body))
-                }
-                wg.Done()
-            }()
-        }
-    }
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "ERROR") {
+			wg.Add(1)
+			go func() {
+				// Sending log to API
+				resp, err := http.Post(apiURL, "application/json", strings.NewReader(line))
+				if err != nil {
+					fmt.Println("Failed to send log:", err)
+				} else {
+					defer resp.Body.Close()
+					body, _ := ioutil.ReadAll(resp.Body)
+					fmt.Printf("Response: %s\n", string(body))
+				}
+				wg.Done()
+			}()
+		}
+	}
 
-    if err := scanner.Err(); err != nil {
-        fmt.Println("Error reading file:", err)
-    }
-    wg.Wait()
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+	wg.Wait()
 }
 
 func main() {
-    readAndSendLogs("server.log", "https://jsonplaceholder.typicode.com/posts")
+	readAndSendLogs("server.log", "https://jsonplaceholder.typicode.com/posts")
 }
-
